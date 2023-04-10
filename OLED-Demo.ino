a0d6c41ce2b81d677f0f3b5a62e377d04ffabd19
Langlois, Vince Thomas <vtlanglo@iu.edu>
2023-04-10 18:00:34 -0400

    added button + counter for demo display

--- OLED-Demo.ino
+++ OLED-Demo.ino
@@ -8,37 +8,62 @@
 
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SSD1306 screen(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+const int buttonPin = 7;
+int buttonState = 0;
+int count = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  //set up button
+  pinMode(buttonPin, INPUT);
+
+  if(!screen.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
 
-  display.display();
+  screen.display();
   delay(2000); // Pause for 2 seconds
 
   // Clear the buffer
-  display.clearDisplay();
+  screen.clearDisplay();
 
-  // Draw a single pixel in white
-  display.drawPixel(10, 10, SSD1306_WHITE);
+  // // Draw a single pixel in white
+  // screen.drawPixel(10, 10, SSD1306_WHITE);
 
-  // Show the display buffer on the screen. You MUST call display() after
-  // drawing commands to make them visible on screen!
-  display.display();
+  // // Show the display buffer on the screen. You MUST call display() after
+  // // drawing commands to make them visible on screen!
+  // screen.display();
 
-  //THIS SHOULD NOT PRINT
-  display.drawPixel(20, 20, SSD1306_WHITE);
-  delay(2000);
+  // screen.drawPixel(20, 20, SSD1306_WHITE);
+
+  // screen.display();
+  // delay(2000);
 
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  screen.setTextSize(1);
+  screen.setTextColor(WHITE);
+  screen.setCursor((SCREEN_WIDTH/2)-40, SCREEN_HEIGHT/2);
+  screen.println("Hello, World!");
+  screen.drawRoundRect(10, 10, (SCREEN_WIDTH-20), SCREEN_HEIGHT-10, 2, WHITE);
+  screen.display();
 
+  buttonState = digitalRead(buttonPin);
+
+  screen.clearDisplay();
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+      count++;
+      Serial.print("pressed!");
+      Serial.println(count);
+      delay(2000);
+  } 
 }
