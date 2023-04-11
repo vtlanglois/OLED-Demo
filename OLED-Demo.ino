2b4bff60a802d4ac363b556c5714acf6e4dfd066
Langlois, Vince Thomas <vtlanglo@iu.edu>
2023-04-11 14:02:09 -0400

    added planet demo + working on getting interrupts to work

--- OLED-Demo.ino
+++ OLED-Demo.ino
@@ -3,77 +3,56 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define OLED_RESET -1        // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C  ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 screen(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-const int buttonPin = 7;
-int buttonState = 0;
-int count = 0;
+const int interruptPin = 2;
+int slide = 0;
+int previousSlide = slide;
+
+int center_x = SCREEN_WIDTH / 2;   // X coordinate of the center
+int center_y = SCREEN_HEIGHT / 2;  // Y coordinate of the center
+float angle = 0;                   // Current angle of the smaller circle for Planet Demo
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   //set up button
-  pinMode(buttonPin, INPUT);
 
-  if(!screen.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+
+  if (!screen.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for (;;)
+      ;  // Don't proceed, loop forever
   }
 
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), test, RISING);
+
   screen.display();
-  delay(2000); // Pause for 2 seconds
+  delay(2000);  // Pause for 2 seconds
 
   // Clear the buffer
   screen.clearDisplay();
-
-  // // Draw a single pixel in white
-  // screen.drawPixel(10, 10, SSD1306_WHITE);
-
-  // // Show the display buffer on the screen. You MUST call display() after
-  // // drawing commands to make them visible on screen!
-  // screen.display();
-
-  // screen.drawPixel(20, 20, SSD1306_WHITE);
-
-  // screen.display();
-  // delay(2000);
-
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  buttonState = digitalRead(buttonPin);
-
-  screen.clearDisplay();
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-      delay(20);
-      // count = (count+1) % 2;
-      Serial.print("pressed!");
-      Serial.println(count);
-      delay(150);
-  } 
-
-  screen.clearDisplay();
-  //based on count, display a demo
-  if(count == 0) {
-    // screen.setTextSize(1);
-    // screen.setTextColor(WHITE);
-    // screen.setCursor((SCREEN_WIDTH/2)-40, SCREEN_HEIGHT/2);
-    // screen.println("Hello, World!");
-    // screen.drawRoundRect(10, 10, (SCREEN_WIDTH-20), SCREEN_HEIGHT-10, 2, WHITE);
+  // screen.setTextSize(1);
+  // screen.setTextColor(WHITE);
+  // screen.setCursor((SCREEN_WIDTH/2)-40, SCREEN_HEIGHT/2);
+  // screen.println("Hello, World!");
+  // screen.drawRoundRect(10, 10, (SCREEN_WIDTH-20), SCREEN_HEIGHT-10, 2, WHITE);
+  if (slide == 0) {
+    previousSlide = slide;
     drawCircleFrame(10, 10, 117, 10, 150);
     drawCircleFrame(12, 9, 115, 11, 125);
     drawCircleFrame(18, 8, 109, 12, 75);
     drawCircleFrame(64, 5, 64, 15, 25);
-    drawCircleFrame(109, 8,  18, 12, 75);
+    drawCircleFrame(109, 8, 18, 12, 75);
     drawCircleFrame(115, 9, 12, 11, 125);
     drawCircleFrame(117, 10, 10, 10, 150);
     drawCircleFrame(115, 11, 12, 9, 125);
@@ -81,18 +60,47 @@ void loop() {
     drawCircleFrame(64, 15, 64, 5, 25);
     drawCircleFrame(18, 12, 109, 8, 75);
     drawCircleFrame(12, 11, 115, 9, 125);
-    // drawCircleFrame(64);
-  } else if (count == 1) {
-    screen.drawPixel(10, 10, WHITE);
+  } else {
+    drawPlanets();
   }
 
-  screen.display();
+  Serial.println(slide);
 }
 
 void drawCircleFrame(int x, int r, int x2, int r2, int pauseLength) {
+  for (int i = 0; i < pauseLength / 30; i++) {
+    if (previousSlide != slide) {
+      Serial.println("JEY!");
+      return;
+    }
     screen.clearDisplay();
     screen.drawCircle(x, 16, r, WHITE);
     screen.drawCircle(x2, 48, r2, WHITE);
     screen.display();
-    delay(pauseLength);
+  }
+}
+
+void drawPlanets() {
+  screen.clearDisplay();
+  // Draw the bigger circle
+  screen.fillCircle(center_x, center_y, big_radius, WHITE);
+
+  // Calculate the position of the smaller circle based on the current angle
+  int x = center_x + cos(angle) * ((big_radius + small_radius) * 1.5);
+  int y = center_y + sin(angle) * ((big_radius + small_radius) * 1.5);
+
+  // Draw the smaller circle
+  screen.fillCircle(x, y, small_radius, WHITE);
+
+  // Update the angle for the next frame
+  angle += 0.1;
+
+  // Show the updated display
+  screen.display();
+}
+
+
+void test() {
+  slide = (slide + 1) % 2;
+  screen.println("AAA");
 }
